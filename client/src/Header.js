8c6ec67aa205fa0835ad27c068ee77eee39de3a0
franaguee <franague99@mail.com>
2020-10-17 00:32:48 -0300

    primer commit

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+  
+import React from 'react';
+
+// header component
+// con title prop
+const Header = ({ title }) => {
+  return (
+    <nav className="justify-content-center navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/#">{title}</a>
+    </nav>
+  );
+};
+
+export default Header;
